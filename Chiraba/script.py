0f9f70feec4c1cf34cf74fe47d3502d58c405d6e
aaron brvenik <aaron.brvenik@threatq.com>
2023-01-05 09:44:20 -0500

    clean up

--- Chiraba/script.py
+++ Chiraba/script.py
@@ -1,22 +1,8 @@
-people = ['Gene','Rahul','Andrew','Kevin','Aaron','Carl','Brendan','Hemachandra']
-
-import requests
 import datetime
 import random
-##import randfacts
+
+people = ['Gene','Rahul','Andrew','Kevin','Aaron','Carl','Brendan','Hemachandra']
 random.seed(str(datetime.date.today()))
 random.shuffle(people)
-# today = datetime.datetime.strftime('%m/%d')
-# url = 'https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all'+ today
-
-# headers = {
-#     'Authorization': 'Bearer: {}'.format(token),
-#     'User-Agent': 'ScrumFacts (aaron.brvenik@threatq.com'
-# }
-# response = requests.get(url, headers).json()
-# response.get()
-# print(list(people))
 for person in people:
     print(person)
-# print()
-# print(randfacts.get_fact())
