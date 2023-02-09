51978881124a44a5d00947953c7fad67f0bc3955
aaron brvenik <aaron.brvenik@threatq.com>
2023-02-09 09:56:00 -0500

    hashes the datetime string to sha-256 in an attempt to be more random

--- Chiraba/script.py
+++ Chiraba/script.py
@@ -1,8 +1,11 @@
 import datetime
 import random
+import hashlib
+hash = hashlib.new('sha256')
+hash.update(str(datetime.date.today()).encode())
 
 people = ['Gene','Rahul','Andrew','Kevin','Aaron','Carl','Brendan','Hemachandra']
-random.seed(str(datetime.date.today()))
+random.seed(hash.hexdigest())
 random.shuffle(people)
 for person in people:
     print(person)
